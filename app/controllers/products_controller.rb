a140af9b87f2517d7af105ef4dd287b8c2d35944
jlbenavente <jlbenaventecanales@gmail.com>
2018-12-10 06:25:32 -0300

    casi finalizado

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,11 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.category_id = params[:category_id]
 
     respond_to do |format|
-      if @product.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+      if @product.save!
+        format.html { redirect_to @product.category, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -13,6 +14,16 @@ class ProductsController < ApplicationController
       end
     end
   end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to @product.category, notice: 'The product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 resources :categories do
-  resources :products, only: [:create]
+  resources :products, only: [:create, :show, :destroy]
 end
   root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
